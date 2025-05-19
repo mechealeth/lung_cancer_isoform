e593d1865d19a59075921cdd3ac5068c6100a4be
mkkk <66460880+mechealeth@users.noreply.github.com>
2025-05-19 13:04:37 +1000

    Update chipseq.sh

--- chipseq.sh
+++ chipseq.sh
@@ -1,22 +1,26 @@
 ### hs1 TSS
-awk '$3=="transcript"' /lustre/tmchen4/ref/Homo_sapiens_chr-GCA_009914755.4-2022_07-genes.gtf | \
-awk 'BEGIN{OFS="\t"} {
-    match($0, /gene_id "[^"]+"/, a); gene_id=a[0];
-    if ($7=="+") { start=$4-3000; end=$4+3000; }
-    else { start=$5-3000; end=$5+3000; }
-    if (start<0) start=0;
-    print $1, start, end, gene_id, ".", $7;
-}' > promoters_TSS_3kb.bed
+
+awk 'BEGIN{OFS="\t"} 
+$3 == "transcript" {
+    match($0, /gene_id "[^"]+"/, gid);
+    gsub(/gene_id "|"/, "", gid[0]);
+    if ($7 == "+") 
+        print $1, $4-1, $4, gid[0], ".", $7; 
+    else if ($7 == "-") 
+        print $1, $5-1, $5, gid[0], ".", $7;
+}' /lustre/tmchen4/ref/Homo_sapiens_chr-GCA_009914755.4-2022_07-genes.gtf > TSS_transcripts.bed
+
 
 computeMatrix reference-point \
-  -S 1HD-H3K4M3-LHC26213_L2_1.srt.nodup.bw 2HD-H3K4M3-LHC26231_L8_1.srt.nodup.bw 3HD-H3K4M3-LHC26248_L8_1.srt.nodup.bw \
-  -R promoters_TSS_3kb.bed \
+  -S 1HI-H3K4M3-LHC26216_L2_1.srt.nodup.bw 3HI-H3K4M3-LHC26253_L8_1.srt.nodup.bw \
+  -R TSS_transcripts.bed \
   --referencePoint TSS -b 3000 -a 3000 -bs 50 --skipZeros \
-  -out matrix_HD_H3K4me3_TSS.gz
+  -out matrix_HI_H3K4me3_TSS.gz
+
 
 plotHeatmap \
-  -m matrix_H3K4me3_TSS.gz \
-  -out H3K4me3_promoter_heatmap.pdf \
+  -m matrix_HD_H3K4me3_TSS.gz \
+  -out matrix_HI_H3K4me3_TSS.gz \
   --colorMap Reds 
 
 #!/bin/bash
