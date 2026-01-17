9e62b50905fa9edc1e982f096a8a76f9ff348f97
mkkk <66460880+mechealeth@users.noreply.github.com>
2026-01-17 14:22:49 +1000

    Update exo_level_quan.sh

--- TRACERX_data/exo_level_quan.sh
+++ TRACERX_data/exo_level_quan.sh
@@ -1,2 +1,9 @@
 cd /scratch/user/uqmche33/Ref
 grep -E 'gene_name "CFAP251"|gene_id "[^"]*CFAP251[^"]*"' Homo_sapiens_chr-GCA_009914755.4-2022_07-genes.gtf > CFAP251.gtf
+
+featureCounts \
+  -a /scratch/user/uqmche33/Ref/CFAP251.gtf \
+  -o tracerx_exon_counts.txt \
+  -f -O \
+  -R BAM \
+  /QRISdata/Q7816/TRACERX_bam/*.bam
