c8924326e5b0132d359ab82936a9e0775b81a82b
mkkk <66460880+mechealeth@users.noreply.github.com>
2026-01-17 14:52:17 +1000

    Update aggr_coverage_plot.sh

--- TRACERX_data/aggr_coverage_plot.sh
+++ TRACERX_data/aggr_coverage_plot.sh
@@ -5,8 +5,8 @@ wiggletools mean $(cat LUAD.txt)   > LUAD.mean.wig
 wiggletools mean $(cat LUSC.txt)   > LUSC.mean.wig
 wiggletools mean $(cat Normal.txt) > Normal.mean.wig
 
-wigToBigWig LUAD.mean.wig   /path/to/chrom.sizes LUAD.mean.bw
-wigToBigWig LUSC.mean.wig   /path/to/chrom.sizes LUSC.mean.bw
-wigToBigWig Normal.mean.wig /path/to/chrom.sizes Normal.mean.bw
+wigToBigWig LUAD.mean.wig   /scratch/user/uqmche33/Ref/hs1.chrom.sizes LUAD.mean.bw
+wigToBigWig LUSC.mean.wig   /scratch/user/uqmche33/Ref/hs1.chrom.sizes LUSC.mean.bw
+wigToBigWig Normal.mean.wig /scratch/user/uqmche33/Ref/hs1.chrom.sizes Normal.mean.bw
 
 
