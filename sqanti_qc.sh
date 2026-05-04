2a32d1b74bf2157294eb25e682e409288fc079c8
mkkk <66460880+mechealeth@users.noreply.github.com>
2026-05-04 21:26:54 +1000

    Update sqanti_qc.sh

--- sqanti_qc.sh
+++ sqanti_qc.sh
@@ -19,3 +19,21 @@ python sqanti3_qc.py  /lustre/tmchen4/iso-seq/cell_line/extend_annotations_corre
                      --CAGE_peak /lustre/tmchen4/iso-seq/SQANTI3-5.2/data/ref_TSS_annotation/hglft_refTSS_v3.1_human_coordinate.hg1.bed \
                      --fl_count /lustre/tmchen4/iso-seq/cell_line/fullLengthCounts_transcript.csv \
                      --cpus 30 --report both
+
+
+
+module load sqanti3/5.2
+python /sw/local/rocky8/noarch/qcif/software/SQANTI3-5.2/sqanti3_qc.py  /QRISdata/Q7820/i3N/bambu/extended_annotations.gtf /scratch/user/uqmche33/Ref/Homo_sapiens_chr-GCA_009914755.4-2022_07-genes.gtf  /scratch/user/uqmche33/Ref/hs1.fa \
+                     --polyA_motif_list /scratch/user/uqmche33/Ref/data/polyA_motifs/mouse_and_human.polyA_motif.txt    \
+                     -o isoseq -d /QRISdata/Q7820/i3N/bambu/SQANTI3_output_hs1SJ.out.tab     \
+		             --force_id_ignore \
+                     --CAGE_peak /scratch/user/uqmche33/Ref/data/ref_TSS_annotation/hglft_refTSS_v3.1_human_coordinate.hg1.bed \
+                     --fl_count /QRISdata/Q7820/i3N/bambu/fullLengthCounts_transcript.csv \
+                     --cpus 40 --report both
+
+
+
+python /sw/local/rocky8/noarch/qcif/software/SQANTI3-5.2hea/sqanti3_filter.py rules -j /lustre/tmchen4/iso-seq/bambu/bambu_NDR0.155/filter_rule.json tissue_isoseq_classification.txt \
+	                           --gtf /lustre/tmchen4/iso-seq/tissue_sample/SQANTI3_output_hs1SJ.out.tab/tissue_isoseq_corrected.gtf \
+                                -o filter
+
